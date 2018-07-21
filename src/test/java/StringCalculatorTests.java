b7b4887dd1a3e82a5f0b6a700662784815bc66dc
Michał Biernacki <micbiernacki@gmail.com>
2018-07-21 13:25:50 +0200

    Adding test "negatives not allowed"

--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -1,3 +1,4 @@
+import jdk.jfr.events.ExceptionThrownEvent;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -8,36 +9,40 @@ import java.security.InvalidParameterException;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTests {
+
+    @Rule
+    public ExpectedException exceptedException = ExpectedException.none();
+
     @Test
-    public void empty_input_returns_zero()  {
+    public void empty_input_returns_zero() {
         String input = "";
         int result = Calc.Sum(input);
-        Assert.assertEquals( 0, result);
+        Assert.assertEquals(0, result);
     }
 
     @Test
-    public void one_number_in_input_returns_number()  {
+    public void one_number_in_input_returns_number() {
         String input = "1";
         int result = Calc.Sum(input);
-        Assert.assertEquals( 1, result);
+        Assert.assertEquals(1, result);
     }
 
     @Test
-    public void one_and_two_in_input_returns_numbers(){
+    public void one_and_two_in_input_returns_numbers() {
         String input = "1,2";
         int result = Calc.Sum(input);
         Assert.assertEquals(3, result);
     }
 
     @Test //2.Metoda add może dodać dowolną ilość liczb
-    public void more_than_two_input_returns_numbers(){
+    public void more_than_two_input_returns_numbers() {
         String input = "1,3,4,6";
         Assert.assertEquals(14, Calc.Sum(input));
     }
 
 
     @Test //3.1  taki string jest poprawny: "1\n2,3" (suma to 6)
-    public void input_with_new_line_returns_sum_of_numbers(){
+    public void input_with_new_line_returns_sum_of_numbers() {
         String input1 = "1\n2\n3";
         String input2 = "1,2\n3";
         String input3 = "1\n2,3";
@@ -56,9 +61,19 @@ public class StringCalculatorTests {
     }*/
 
     @Test //2.Metoda add może dodać dowolną ilość liczb
-    public void input_with_new_separators_returns_sum_of_numbers(){
+    public void input_with_new_separators_returns_sum_of_numbers() {
         String input = "//;\n1;2";
         Assert.assertEquals(3, Calc.Sum(input));
+    }
+
+    @Test
+    public void input_negative_returns_negativesNotAllowdedException_and_all_negative_numbers() {
+        String input = "-1,2,-3,4";
+
+        exceptedException.expect(InvalidParameterException.class);
+        exceptedException.expectMessage("negatives not allowed. Negative numbers: -1, -3");
+        Calc.Sum(input);
+
     }
 
 
